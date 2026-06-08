7fc4f353e4d884cc796fdde200d343229e4030fd
deepanshugarg0403 <deepanshugargkumar@gmail.com>
2026-06-08 23:11:41 +0530

    Updated project

---
+++ green.js
@@ -0,0 +1,9 @@
+greenBtn.addEventListener("click", function(){
+
+    const boxes = document.querySelectorAll(".shape-box");
+
+    boxes.forEach(function(box){
+        box.style.backgroundColor = "green";
+    });
+
+});
